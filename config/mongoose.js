7ddeb4d77e51c625fdd25cf81acd338868699f03
rahuls0008 <rahulksingh0008@gmail.com>
2021-12-19 20:12:29 +0530

    finish Setting up the Directory Structure (scalable)

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,22 @@
 //npm install mongoose
 
+//connecting to mongoDB using mongoose  //read mongoose docs
 const mongoose = require('mongoose');
 
+//connect to the database
 mongoose.connect('mongodb://localhost/codeial_development');
 
-const db = mongoose.connection;
+//acquire the connection (to check if it is successfull)
+const db = mongoose.connection;         //db to access the database
 
+//if error
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
+//up and running 
 db.once('open', function(){
     console.log('Connected to Database :: MongoDB');
 });
 
+
+//export the module
 module.exports = db;
