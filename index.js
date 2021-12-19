a06235a9bde7b30674bc5592882aecade08cba5b
rahuls0008 <rahulksingh0008@gmail.com>
2021-12-19 19:18:16 +0530

    create Layout & implement Partials

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');   //for layouts : npm install express-ejs-layouts 
+
+app.use(expressLayouts);       //MUST BE CALLED BEFORE ROUTES
 
 //use express router
 app.use('/', require('./routes'));        //home router
 
+//set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
