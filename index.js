f8f7332db6dc1945f1b544bab9fa7fdbe1decb99
rahuls0008 <rahulksingh0008@gmail.com>
2021-12-19 19:56:18 +0530

    set up Static Files Access

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');   //for layouts : npm install express-ejs-layouts 
 
-app.use(expressLayouts);       //MUST BE CALLED BEFORE ROUTES
+//tell the app to use Layout
+app.use(expressLayouts);       //LAYOUTS MUST BE CALLED BEFORE ROUTES
+
+//use static files
+app.use(express.static('./assets'));
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 //use express router
 app.use('/', require('./routes'));        //home router
