24c4a518aa7ddc951b76bd6f4efc034def851333
rahuls0008 <rahulksingh0008@gmail.com>
2021-12-20 20:07:52 +0530

    create sign-up & sign-in forms And also create actions for respective routes in user_controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,6 @@
 
 //Actions
+
 // render the user > profile page
 module.exports.profile = function(req, res){
     // return res.end('<h1>  User Profile   </h1>');
@@ -20,4 +21,15 @@ module.exports.signIn = function(req, res){
     return res.render('user_sign_in',{
         title : "Codeial | Sign In"
     });
+}
+
+
+//get the sign up data
+module.exports.create = function(req, res){
+    //TODO Later
+}
+
+//sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    //TODO Later
 }
