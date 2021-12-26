b5803a5c1c9320381e7f2e9cf91e22d442bc6106
rahuls0008 <rahulksingh0008@gmail.com>
2021-12-26 21:33:58 +0530

    restrict the accessibility of sign-in and sign-up pages to  only when user is signed-out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -14,6 +14,12 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+
+    //restrict the accessibilty of sign-up page to only when user is signed out
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up',{
         title : "Codeial | Sign Up"
     });
@@ -21,6 +27,12 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+
+    //restrict the accessibilty of sign-in page to only when user is signed out
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in',{
         title : "Codeial | Sign In"
     });
