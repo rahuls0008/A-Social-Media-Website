6f15eabe2428ff2d8a6e314fd6fc99cb72ee3e63
rahuls0008 <rahulksingh0008@gmail.com>
2022-03-26 21:14:13 +0530

    display the Posts & related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,17 +7,43 @@
             // router.get('/', homeController.actionName);
 
 
+//import post schema
+const Post = require('../models/post');
+
+
 
 //Home Action
 module.exports.home = function(req, res){
     // return res.end('<h1>  Express is up for Codeal   </h1>');
-    console.log(req.cookies);
-  
+    // console.log(req.cookies);
     
-    
-    return res.render('home',{
-        title : "home"
+
+    // display the Posts
+
+    // Post.find({}, function(err, posts){                       //find({query}) - find all posts from db
+    //     return res.render('home',{
+    //         title : "Codeial | home",
+    //         posts : posts          //pass the posts to home page
+    //     });
+    // });
+
+
+    // display the posts & related user
+
+    //find({}) - find all posts from db  > populate the user of each post 
+                                        // (because we only had "user : user_id" in db > we populate user to get all other user values(name etc.))
+    Post.find({}).populate('user').exec(function(err, posts){    
+        return res.render('home',{
+            title : "Codeial | home",
+            posts : posts          //pass the posts to home page
+        });
     });
+    
+
+
+    // return res.render('home',{
+    //     title : "home"
+    // });
 }
 
 
